7c9acf9d2cce7818813986c6387bfe04bd42a952
Vitalii Svinchyak <vitalii.svinchyak@andersenlab.com>
2017-08-22 17:45:59 +0300

    improvements for input parsing

--- content-scripts/formManager.js
+++ content-scripts/formManager.js
@@ -7,23 +7,57 @@ class FormManager {
         this.changeLog = {};
     }
 
-    logInputChange(element) {
+    logInputChange(element, disabled) {
+        disabled = disabled || false;
         const label = this.detectLabel(element);
         const date = new Date();
         const hours = Helper.generateZerosBeforeNumber(date.getHours(), 2);
         const minutes = Helper.generateZerosBeforeNumber(date.getMinutes(), 2);
         const seconds = Helper.generateZerosBeforeNumber(date.getSeconds(), 2);
         const time = `${hours}:${minutes}:${seconds}`;
+        const value = this.getElementValue(element);
 
         this.changeLog[this.getPathTo(element)] = {
             label: label,
-            value: element.value,
-            time: time
+            value: value,
+            time: time,
+            disabled: disabled
         };
+    }
+
+    getElementValue(element) {
+        switch (element.tagName) {
+            case 'TEXTAREA':
+            case 'INPUT':
+                switch (element.type) {
+                    case 'checkbox':
+                        return element.checked ? 'Yes' : 'No';
+                    case 'radio':
+                        if (element.parentNode.tagName === 'LABEL') {
+                            return element.parentNode.textContent.trim();
+                        }
+                    default:
+                        return element.value;
+                }
+            case 'SELECT':
+                for (const option of element.options) {
+                    if (option.value === element.value) {
+                        return option.textContent.trim();
+                    }
+                }
+
+                if (element.options[0]) {
+                    return element.options[0].textContent.trim();
+                }
+            default:
+                return '<strong>WTF?! Can\'t understand!</strong>';
+
+        }
     }
 
     detectLabel(element) {
         let label;
+        let parentContainer = element.parentNode;
 
         if (element.id) {
             label = document.querySelector(`label[for="${element.id}"]`);
@@ -33,7 +67,10 @@ class FormManager {
             return label.textContent;
         }
 
-        let parentContainer = element.parentNode;
+        if (element.type === 'radio') {
+            parentContainer = element.parentNode.parentNode.parentNode.parentNode;
+        }
+
         for (let i = 0; i <= 5; i++) {
             const oneLabel = this.getOneLabelForTwoFields(parentContainer);
             if (oneLabel) {
@@ -95,19 +132,52 @@ class FormManager {
         return null;
     }
 
+    getInputElements() {
+        let addionalQuery = '';
+        if (document.querySelector('.todo_col2')) {
+            addionalQuery = '.todo_col2';
+        }
+
+        let elements = Array.prototype.slice.call(
+            document.querySelectorAll(`${addionalQuery} input:not(disabled):not([type=hidden]):not([type=file]),
+                ${addionalQuery} select:not(disabled),${addionalQuery} textarea:not(disabled)`)
+        );
+
+        elements = elements.filter(element => {
+            if (element.type === 'radio') {
+                return element.checked;
+            }
+            return true;
+        });
+
+        return elements;
+    }
+
     detectFormChanges() {
         this.changeLog = {};
-        const elements = document.querySelectorAll(`input:not(disabled):not([type=hidden]):not([type=file]),
-        select:not(disabled),textarea:not(disabled)`);
+        const elements = this.getInputElements();
 
         for (const element of elements) {
             if (element.attributes.value && element.attributes.value.value !== element.value) {
-                this.logInputChange(element);
+                this.logInputChange(element, false);
+            }
+        }
+    }
+
+    detectNotChangedFields() {
+        const elements = this.getInputElements();
+
+        for (const element of elements) {
+            const hash = this.getPathTo(element);
+
+            if (!this.changeLog.hasOwnProperty(hash)) {
+                this.logInputChange(element, true);
             }
         }
     }
 
     showFormChanges() {
+        this.detectNotChangedFields();
         let changes = '';
 
         for (const changeKey in this.changeLog) {
@@ -116,7 +186,8 @@ class FormManager {
             }
 
             const change = this.changeLog[changeKey];
-            changes += `<tr hash="${changeKey}">
+            const disabledClass = change.disabled ? 'class="disabled"' : '';
+            changes += `<tr ${disabledClass} hash="${changeKey}">
                                             <td>${change.time}</td>
                                             <td>input</td>
                                             <td>${change.label}</td>
@@ -129,6 +200,7 @@ class FormManager {
 
     generateTextWithChanges() {
         let changes = '';
+        let rows = 1;
 
         for (const changeKey in this.changeLog) {
             if (!this.changeLog.hasOwnProperty(changeKey)) {
@@ -139,7 +211,7 @@ class FormManager {
             if (change.disabled) {
                 continue;
             }
-
+            rows++;
             changes += `${change.label} : ${change.value} \n`;
         }
 
@@ -149,9 +221,9 @@ class FormManager {
 
         if (textarea) {
             textarea.textContent = changes;
+            textarea.attributes.rows.value = rows;
+            textarea.focus();
         } else {
-            const rows = Object.getOwnPropertyNames(this.changeLog).length + 1;
-
             const html = `<textarea autofocus class="form-changes" rows="${rows}">${changes}</textarea>`;
             formChangesPopup.insertAdjacentHTML('beforeend', html);
         }
@@ -159,20 +231,19 @@ class FormManager {
     }
 
     insertFormChanges(data) {
-        const html = `
-                            <table>
-                                <thead>
-                                <tr>
-                                    <th>Время</th>
-                                    <th>Объект</th>
-                                    <th>Заголовок</th>
-                                    <th>Значение</th>
-                                </tr>
-                                </thead>
-                                ${data}
-                                </tbody>
-                            </table>
-                            <button class="exportButton">Сгенерировать</button>`;
+        const html = `<table>
+                        <thead>
+                        <tr>
+                            <th>Время</th>
+                            <th>Объект</th>
+                            <th>Заголовок</th>
+                            <th>Значение</th>
+                        </tr>
+                        </thead>
+                        ${data}
+                        </tbody>
+                    </table>
+                    <button class="exportButton">Сгенерировать</button>`;
         Helper.renderModal(html);
 
         document.querySelector('erp-helper-modal table').addEventListener('click', e => {
